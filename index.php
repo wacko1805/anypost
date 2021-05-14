2d252edab4de1b10e1c24220a4bfb69fe917cd5e
Jack Sam <54708993+wacko1805@users.noreply.github.com>
2021-05-14 03:13:14 -0700

    Update index.php

--- index.php
+++ index.php
@@ -38,32 +38,7 @@
 
 <main>
      <?php include("posts.php");?>
-<?php
-$path='posts/';
-function scan_dir($dir) {
-
-    $files = array();    
-    foreach (scandir($dir) as $file) {
-        $files[$file] = filemtime($dir . '/' . $file);
-    }
-
-    arsort($files);
-    $files = array_keys($files);
-
-    return ($files) ? $files : false;
-}
-$files=scan_dir($path);
-
-
-foreach ($files as $key => $value) {
-    if($value!="." && $value!="..")
-    {
-       
-        print_r (file_get_contents($path."/" .$value));
-        echo "</p></article></article><br><br>";
-    }
-}
-?></main>
+</main>
 <footer>
   <div class="mdc-bottom-navigation">
         <nav class="mdc-bottom-navigation__list">
