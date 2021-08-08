85afc1c7eb24e9afbc23acc98b23a8481e6b35c6
Jack Sam <54708993+wacko1805@users.noreply.github.com>
2021-08-09 00:29:26 +1000

    Added spam blocking and topics

--- index.php
+++ index.php
@@ -21,10 +21,15 @@
       <textarea  maxlength="500" rows="7"  type="text" id="comment" name="comment"  placeholder="Comment" required autocomplete="off" ></textarea> <br>
       <input style="display:none;" type="text" name="date" value="<?= $date ?>">
       <input style="display:none;" type="text" name="displaydate" value="<?= $displaydate ?> <?= $timezone ?>">
+      <select id="topic" name="topic">
+      <option id="no topic">No Topic</option>
+      <?php include("select-posts.php");?>
+  </select>
       <div  class="submitp"><p><input type="submit" id='submit'   name="submit" value="Submit"><i>By submitting,<br> you agree to the <br><a href="t+c.php#main">Terms and conditions</a></i></p></div>
     </div>
 </form>
-
+<script src="/assets/js/disable.js">
+</script>
   
 <script>
   $(document).ready(function() {
