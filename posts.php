df21542127fb0c22f66a39ab15d959c0edb118e5
Jack Sam <54708993+wacko1805@users.noreply.github.com>
2021-08-09 11:10:39 +1000

    Added icon when post has comment

--- posts.php
+++ posts.php
@@ -1,5 +1,10 @@
 
+
 <?php
+// Check connection
+
+
+
 
 
 
@@ -17,6 +22,7 @@ die("Connection failed: " . $conn->connect_error);
 
 $sql = "SELECT * FROM `posts` ORDER BY `posts`.`upload_time` DESC ";
 
+
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
@@ -24,7 +30,21 @@ if ($result->num_rows > 0) {
 
 while($row = $result->fetch_assoc()) {
 
-echo " " . "<a  href='big.php?id=" . $row["id"]. "'><article><h2> " . $row["title"]. "</h2><p> " . $row["comment"]. "</p><div class='topics'   id='topics'><a href='topics-post.php?topic=" . $row["topic"]. "'><p class='topics-hide' >" . $row["topic"]. "</p></div></a><a  href='big.php?id=" . $row["id"]. "'><br><div class='more'><i class='far fa-comments'></i></a></div><span>" . $row["display_time"]. " </span></artilce></article></div><br>";
+    // Checks to see if comments exist
+    $sql_com = "SELECT post_id FROM comments WHERE `post_id`='" . $row["id"]. "'";
+    $result_com = $conn->query($sql_com);
+    
+    if ($result_com->num_rows > 0) {
+        // output data of each row
+        while($row_com = $result_com->fetch_assoc()) {
+            $comments_ave = "<i class='far fa-comments'>";
+        }
+    } else {
+        $comments_ave = "";
+    }
+echo " " . "<a  href='big.php?id=" . $row["id"]. "'><article><h2> " . $row["title"]. "</h2><p> " . $row["comment"]. "</p><div class='topics'   id='topics'><a href='topics-post.php?topic=" . $row["topic"]. "'><p class='topics-hide' >" . $row["topic"]. "</p></div></a><a  href='big.php?id=" . $row["id"]. "'><br><div class='more'><i class='fas fa-reply'></i> ";
+echo $comments_ave;
+echo "</i></a></div><span>" . $row["display_time"]. " </span></artilce></article></div><br>";
 }
 
 } else {
--- topics-post.php
+++ topics-post.php
@@ -34,15 +34,29 @@ $topic = $row['topic'];
 </div>
 <?php
 
-$query="SELECT * FROM `posts`  WHERE `topic`='$topic'";
+$query="SELECT * FROM `posts`  WHERE `topic`='$topic' ORDER BY `posts`.`upload_time` DESC ";
 $results = mysqli_query($conn, $query) or exit(mysqli_error());
 
-while ($row = mysqli_fetch_array($results)) {
+while($row = $results->fetch_assoc()) {
 
-//show only object_id, name and wish
-echo " " . "<a  href='big.php?id=" . $row["id"]. "'><article><h2> " . $row["title"]. "</h2><p> " . $row["comment"]. "</p><a  href='big.php?id=" . $row["id"]. "'><div class='more'><i class='far fa-comments'></i></a></div><span>" . $row["display_time"]. " </span></artilce></article></div><br>";
-
+  // Checks to see if comments exist
+  $sql_com = "SELECT post_id FROM comments WHERE `post_id`='" . $row["id"]. "'";
+  $result_com = $conn->query($sql_com);
+  
+  if ($result_com->num_rows > 0) {
+      // output data of each row
+      while($row_com = $result_com->fetch_assoc()) {
+          $comments_ave = "<i class='far fa-comments'>";
+      }
+  } else {
+      $comments_ave = "";
+  }
+echo " " . "<a  href='big.php?id=" . $row["id"]. "'><article><h2> " . $row["title"]. "</h2><p> " . $row["comment"]. "</p><div class='topics'   id='topics'><a href='topics-post.php?topic=" . $row["topic"]. "'><p class='topics-hide' >" . $row["topic"]. "</p></div></a><a  href='big.php?id=" . $row["id"]. "'><br><div class='more'><i class='fas fa-reply'></i> ";
+echo $comments_ave;
+echo "</i></a></div><span>" . $row["display_time"]. " </span></artilce></article></div><br>";
 }
+
+
 ?>
 
 </body>
