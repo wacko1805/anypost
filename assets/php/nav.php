330ead848f65a9b91f2ea045bc7c02835dcd4836
Jack Sam <54708993+wacko1805@users.noreply.github.com>
2021-08-23 09:40:46 +1000

    Added random posts

--- assets/php/nav.php
+++ assets/php/nav.php
@@ -1,19 +1,5 @@
 <div id="top"></div>
-
 <div id="header"><?php echo $maintitle ?><span id="span">.</span></div>
-<div class="Topnav"  id="myTopnav">
-<a class="top" href="../" >anypost</a>
-
-
-        <a href="javascript:void(0);" class="icon" onclick="fullnav()">
-          <i class="fa fa-bars"></i>
-        </a>
-        <div class="progress-container">
-    <div class="progress-bar" id="myBar"></div>
-  </div>  
-      </div>
-
-
       <header class="header">
         <a href="../" class="logo">AnyPost</a>
         <input class="menu-btn" type="checkbox" id="menu-btn" />
@@ -21,10 +7,10 @@
         <ul class="menu">
           <li><a href=".">Home</a></li>
           <li><a href="/topics.php">Topics</a></li>
+          <li><a href="/random.php">Random Post</a></li>
           <li><a href="../download.php">Download APK</a></li>
           <li><a href="/t+c.php">terms and conditions</a></li>
           <li><a href="https://github.com/wacko1805/anypost">source code</a></li>
         </ul>
       </header>
-
 </div>
---
+++ random.php
@@ -0,0 +1,82 @@
+<?php
+// Check connection
+
+
+
+include('config.php');
+
+
+$conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
+
+
+if ($conn->connect_error) {
+
+die("Connection failed: " . $conn->connect_error);
+
+
+}
+
+
+
+$sql = "SELECT * from posts ORDER BY RAND() LIMIT 0,1;";
+
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+
+
+while($row = $result->fetch_assoc()) {
+
+    // Checks to see if comments exist
+
+    $id = $row["id"];
+    $title = $row["title"];
+    $comment = $row["comment"];
+    $date = $row["display_time"];
+    $topic = $row["topic"];
+
+    
+}
+
+} else {
+
+echo "<article><h2>Theres no posts!</h2></article>";
+
+}
+
+$conn->close();
+
+?>
+<html>
+    <head>
+    <title><?php echo $title; ?> | Random Post - Anypost</title>
+    </head>
+    <body>
+    <?php include("assets/php/nav.php");?>
+<main>
+        <div class="random-post">
+            <div class="main">
+                <h2><?php echo $title; ?></h2>
+                <p><?php echo $comment; ?></p>
+            </div>
+            <div class="badge">
+                <a id="time" ><?php echo $date; ?></a>
+                <a id="topics" class="topics-hide" href="#"><?php echo $topic; ?></a>
+                <a id="comment" href="big.php?id=<?php echo $id; ?>">Comment</a>
+            </div>
+        </div class="random-post">
+        <div class="buttons"><a onClick="window.location.reload();"><buttton class="badge"><i class="fas fa-redo"></i> New Post</button></a>
+        </main>
+        <script>
+   let attribute = document.getElementsByClassName('topics-hide');
+   for (let i = 0; i < attribute.length; i++) {
+      let impDiv = attribute[i];
+      let value = impDiv.innerHTML.trim();
+      if (value == 'No Topic' || value == '') {
+         impDiv.style.display = 'none';
+      }
+   }
+</script>
+
+</body>
